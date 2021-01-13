762cb37dbfcc09d2d48db00bc4d30706eb17b73f
Your Name <you@example.com>
2021-01-13 13:35:13 +0100

    page de connexion inventaire

---
+++ application/views/connexion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./theme/assets/icone/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?= theme_url() ?>assets/css/connexion.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <main>
+        <div class="contenair">
+            <div class="header">  inventaire <br> G043 </div>
+               
+           
+            <form action="">
+                <label for="identifiant">identifiant</label>
+                <input type="text" name="" id="identifiant">
+
+                <label for="mdp">mot de passe</label>
+                <input type="text" name="" id="mdp">
+                <button type="submit">se connecter</button>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
