1c7ea90fb52a4c9670845a551329fc369439ab2b
nick <nickqadosh@gmail.com>
2021-01-13 08:50:27 +0100

    fonction pour quantite

---
+++ application/models/Quantite_model.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Produit_model extends CI_Model
+{   
+    public $q_surf;
+    public $q_res;
+    public $s_surf;
+    public $s_res;
+    public $folio;
+
+    // Nom de la table
+    private $table = 'quantite';
+
+    // Cle primaire de la table
+    private $id = 'id_quant';
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    // Recoit un tableau qui contient les valeurs pour sauvegarder des quantités
+    public function creer($params)
+    {
+       return $this->db->insert($this->table, $params);
+        
+    }
+
+    public function rechercher($id)
+    {
+        $query = $this->db->get_where($this->table, array($this->id => $id));
+        return $query->row();
+    }
+}
+
+?>
