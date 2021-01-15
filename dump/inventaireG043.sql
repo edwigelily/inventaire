4e8a07dc52839b42627418343c84a7a6f7c699dc
edwigelily <bamenouedwige@gmail.com>
2021-01-15 09:02:37 +0100

    Ajout de l'attribut h_gamme BOOLEAN DEFAULT true

--- dump/inventaireG043.sql
+++ dump/inventaireG043.sql
@@ -277,8 +277,9 @@ CREATE TABLE `produit` (
   `id_prod` int NOT NULL AUTO_INCREMENT,
   `folio` int NOT NULL,
   `libelle_prod` varchar(255) NOT NULL,
-  `code_fam` int NOT NULL,
+  `code_fam` int DEFAULT NULL,
   `prix` int NOT NULL,
+  `h_gamme` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`folio`),
   UNIQUE KEY `id_prod` (`id_prod`),
   KEY `FK_familleProduit` (`code_fam`),
@@ -359,4 +360,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-12 11:10:01
+-- Dump completed on 2021-01-15  9:01:28
