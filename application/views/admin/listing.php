cce3d29a19bb8d3fa47ef677137eb2c1cadec39b
dynamo63 <smoulongui@gmail.com>
2021-01-17 10:41:07 +0100

    Actualisation 5 minute admin

--- application/views/admin/listing.php
+++ application/views/admin/listing.php
@@ -11,6 +11,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?= theme_url() ?>assets/css/listing_admin.css">
     <title>Liste des produits ~ Dashboard Admin</title>
+    <script>
+        setInterval(() => {
+            location.reload()
+        }, 5 * 60 * 1000);
+    </script>
 </head>
 
 <body>
